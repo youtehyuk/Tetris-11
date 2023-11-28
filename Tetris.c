b15c0f5c871e6c1750d575c598fc7fd3f16e65ed
youtehyuk <144311058+youtehyuk@users.noreply.github.com>
2023-11-28 16:31:38 +0900

    테트리스

---
+++ Tetris.c
@@ -0,0 +1,281 @@
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h> 
+#include <windows.h>
+#include <time.h>
+
+#define randomize() srand((unsigned)time(NULL))
+#define random(n) (rand() % (n))
+#define delay(n) Sleep(n)
+#define clrscr() system("cls")
+#define gotoxy(x,y) { COORD Cur = {x, y}; \
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),Cur);}
+#define showcursor(bShow) { CONSOLE_CURSOR_INFO CurInfo = {20, bShow}; \
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&CurInfo); }
+
+
+enum { ESC = 27, LEFT = 75, RIGHT = 77, UP = 72, DOWN = 80 };
+#define putsxy(x, y, s) {gotoxy(x, y);puts(s);}
+#define BX 5
+#define BY 1
+#define BW 10
+#define BH 20
+
+void DrawScreen();
+BOOL ProcessKey();
+void PrintBrick(BOOL Show);
+int GetAround(int x, int y, int b, int r);
+BOOL MoveDown();
+void TestFull();
+void write_file();
+void read_file();
+void InitializeGameTime();
+int GetGameTime();
+void color(int c);
+
+struct Point {
+	int x, y;
+};
+struct Point Shape[][4][4] = {
+	{ { 0,0,1,0,2,0,-1,0 },{ 0,0,0,1,0,-1,0,-2 },{ 0,0,1,0,2,0,-1,0 },{ 0,0,0,1,0,-1,0,-2 } },
+	{ { 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 } },
+	{ { 0,0,-1,0,0,-1,1,-1 },{ 0,0,0,1,-1,0,-1,-1 },{ 0,0,-1,0,0,-1,1,-1 },{ 0,0,0,1,-1,0,-1,-1 } },
+	{ { 0,0,-1,-1,0,-1,1,0 },{ 0,0,-1,0,-1,1,0,-1 },{ 0,0,-1,-1,0,-1,1,0 },{ 0,0,-1,0,-1,1,0,-1 } },
+	{ { 0,0,-1,0,1,0,-1,-1 },{ 0,0,0,-1,0,1,-1,1 },{ 0,0,-1,0,1,0,1,1 },{ 0,0,0,-1,0,1,1,-1 } },
+	{ { 0,0,1,0,-1,0,1,-1 },{ 0,0,0,1,0,-1,-1,-1 },{ 0,0,1,0,-1,0,-1,1 },{ 0,0,0,-1,0,1,1,1 } },
+	{ { 0,0,-1,0,1,0,0,1 },{ 0,0,0,-1,0,1,1,0 },{ 0,0,-1,0,1,0,0,-1 },{ 0,0,-1,0,0,-1,0,1 } },
+};
+
+enum { EMPTY, BRICK, WALL };
+char arTile[3][4] = { ". ","��","��" };
+int board[BW + 2][BH + 2];
+int nx, ny;
+int brick, rot;
+int score = 0, max_score = 0;
+time_t startTime;
+
+int main()
+{
+	int nFrame, nStay;
+	int x, y;
+
+	showcursor(FALSE);
+	randomize();
+	clrscr();
+	read_file();
+	InitializeGameTime();
+	// �����ڸ��� ��, �������� �� �������� �ʱ�ȭ�Ѵ�.
+	for (x = 0; x < BW + 2; x++) {
+		for (y = 0; y < BH + 2; y++) {
+			if (y == 0 || y == BH + 1 || x == 0 || x == BW + 1) {
+				board[x][y] = WALL;
+			}
+			else {
+				board[x][y] = EMPTY;
+			}
+		}
+	}
+
+	DrawScreen();
+	nFrame = 20;
+
+	// ��ü ���� ����
+	for (; 1;) {
+
+		brick = random(sizeof(Shape) / sizeof(Shape[0]));
+
+		nx = BW / 2;
+		ny = 3;
+		rot = 0;
+		PrintBrick(TRUE);
+
+		if (GetAround(nx, ny, brick, rot) != EMPTY) break;
+
+		// ���� �ϳ��� �ٴڿ� ���� �������� ����
+		nStay = nFrame;
+		for (; 2;) {
+			if (--nStay == 0) {
+				nStay = nFrame;
+				if (MoveDown()) break;
+			}
+			if (ProcessKey()) break;
+			delay(1000 / 20);
+		}
+	}
+	clrscr();
+	putsxy(30, 12, "G A M E  O V E R");
+	gotoxy(30, 12);
+	printf("���� ����: %d�� �ְ� ���: %d", score, max_score);
+	showcursor(TRUE);
+}
+
+void DrawScreen()
+{
+	for (int x = 0; x < BW+2; x++) {
+		for (int y = 0; y < BH + 2; y++) {
+			putsxy(BX + x * 2, BY + y, arTile[board[x][y]]);
+		}
+	}
+
+	read_file();
+	putsxy(50, 3, "Tetris Ver 1.0");
+	putsxy(50, 5, "�¿�:�̵�, ��:ȸ��, �Ʒ�:����");
+	putsxy(50, 6, "����:���� ����");
+	gotoxy(50, 7);
+	printf("���� ����: %d", score);
+	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+	gotoxy(50, 8);
+	printf("�ְ� ���: %d", max_score);
+
+}
+
+BOOL ProcessKey()
+{
+	if (kbhit()) {
+		int ch = getch();
+		if (ch == 0xE0 || ch == 0) {
+			ch = getch();
+			switch (ch) {
+			case LEFT:
+				if (GetAround(nx - 1, ny, brick, rot) == EMPTY) {
+					PrintBrick(FALSE);
+					nx--;
+					PrintBrick(TRUE);
+				}
+				break;
+			case RIGHT:
+				if (GetAround(nx + 1, ny, brick, rot) == EMPTY) {
+					PrintBrick(FALSE);
+					nx++;
+					PrintBrick(TRUE);
+				}
+				break;
+			case UP:
+				if (GetAround(nx, ny, brick, (rot + 1) % 4) == EMPTY) {
+					PrintBrick(FALSE);
+					rot = (rot + 1) % 4;
+					PrintBrick(TRUE);
+				}
+				break;
+			case DOWN:
+				if (MoveDown()) {
+					return TRUE;
+				}
+				break;
+			}
+		}
+		else {
+			switch (ch) {
+			case ' ':
+				while (MoveDown() == FALSE) { ; }
+				return TRUE;
+			case ESC:
+				exit(0);
+			}
+		}
+	}
+	return FALSE;
+}
+
+void PrintBrick(BOOL Show)
+{
+	gotoxy(50, 9);
+	printf("���� �ð�: %d��", GetGameTime());
+	for (int i = 0; i < 4; i++) {
+		gotoxy(BX + (Shape[brick][rot][i].x + nx) * 2, BY + Shape[brick][rot][i].y + ny);
+		puts(arTile[Show ? BRICK : EMPTY]);
+	}
+}
+
+int GetAround(int x, int y, int b, int r)
+{
+	int k = EMPTY;
+
+	// ������ ������ Ÿ�� ��� �� ���� ū ���� ã�´�.
+	for (int i = 0; i < 4; i++) {
+		k = max(k, board[x + Shape[b][r][i].x][y + Shape[b][r][i].y]);
+	}
+	return k;
+}
+
+BOOL MoveDown()
+{
+	// �ٴڿ� ������� ����á���� �����ϰ� TRUE�� �����Ѵ�.
+	if (GetAround(nx, ny + 1, brick, rot) != EMPTY) {
+		TestFull();
+		return TRUE;
+	}
+	// ���� ���߿� �� ������ ��ĭ �Ʒ��� ������.
+	PrintBrick(FALSE);
+	ny++;
+	PrintBrick(TRUE);
+	return FALSE;
+}
+
+void TestFull()
+{
+	// �ٴڿ� �������� ���� ���
+	for (int i = 0; i < 4; i++) {
+		board[nx + Shape[brick][rot][i].x][ny + Shape[brick][rot][i].y] = BRICK;
+	}
+
+	// �������� ������ ���� ����
+	for (int y = 1; y < BH + 1; y++) {
+		BOOL bFull = TRUE;
+		for (int x = 1; x < BW + 1; x++) {
+			if (board[x][y] == EMPTY) {
+				bFull = FALSE;
+				break;
+			}
+		}
+		// ������ ���� á���� �� ���� �����Ѵ�.
+		if (bFull) {
+			for (int ty = y; ty > 1; ty--) {
+				for (int x = 1; x < BW+1; x++) {
+					board[x][ty] = board[x][ty - 1];
+				}
+			}
+			score += 1000;
+			if (score > max_score) {
+				write_file();
+			}
+			DrawScreen();
+			delay(200);
+		}
+	}
+}
+
+
+void write_file() {
+#pragma warning(disable:4996)
+	FILE* fp;
+	fp = fopen("max_score.txt", "w+");
+	fwrite(&score, sizeof(int), 1, fp);
+	fclose(fp);
+}
+
+void read_file()
+{
+#pragma warning(disable:4996)
+	FILE* fp;
+	fp = fopen("max_score.txt", "r + ");
+	if (fp == NULL)
+	{
+		fp = fopen("max_score.txt", "w + ");
+		fwrite(&max_score, sizeof(int), 1, fp);
+	}
+	fseek(fp, 0, 0);
+	fread(&max_score, sizeof(int), 1, fp);
+	fclose(fp);
+}
+
+void InitializeGameTime() {
+	startTime = time(NULL);
+}
+
+// ���� ���� ��� �ð��� ��ȯ�ϴ� �Լ�
+int GetGameTime() {
+	time_t currentTime = time(NULL);
+	return (int)(currentTime - startTime);
+}
